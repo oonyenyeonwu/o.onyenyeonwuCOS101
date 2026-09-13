224030b71af58afec4d437371a8a307a4514ce8a
oonyenyeonwu <ogochukwu.onyenyeonwu@pau.edu.ng>
2026-09-13 23:34:00 +0100

    Immutables

---
+++ week-3/practice_8/src/main.rs
@@ -0,0 +1,5 @@
+fn main() {
+    let fees = 25_000;
+
+    println!("Fees = {}", fees);
+}
---
+++ week-3/practice_8/target/debug/practice_8.d
@@ -0,0 +1,1 @@
+C:\Users\hp\Documents\o.onyenyeonwuCOS101\week-3\practice_8\target\debug\practice_8.exe: C:\Users\hp\Documents\o.onyenyeonwuCOS101\week-3\practice_8\src\main.rs
