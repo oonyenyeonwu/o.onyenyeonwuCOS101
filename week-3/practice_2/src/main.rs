41d226277a30a756f6714f812b91ddea9aca27f0
oonyenyeonwu <ogochukwu.onyenyeonwu@pau.edu.ng>
2026-09-13 23:01:32 +0100

    Integers

---
+++ week-3/practice_2/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let result = 10;
+    let age: u32 = 20;
+    let sum: i32 = 5 - 15;
+
+    println!("result = {}", result);
+    println!("age = {}", age);
+    println!("sum = {}", sum);
+}
